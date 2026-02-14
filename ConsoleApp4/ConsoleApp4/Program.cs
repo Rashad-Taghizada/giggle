de921e3c2bdf80d554c202e7b28a483c86d509b5
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-14 23:20:18 +0100

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+//Vehicle class-
+//Color -
+
+//Year
+
+//ps: Buraxilis ili teyin olunmamis Vehicle ola bilmez
+
+//Model-
+
+//FuelCapacity-
+
+//FuelFor1Km-
+
+//CurrentFuel-
+
+//ShowInfo()-butun detallari gosterecek
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void ShowInfo(string info)
+        {
+            Console.WriteLine(info); 
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Vehicleclass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Vehicle class-
+//Color -
+
+//Year
+
+//ps: Buraxilis ili teyin olunmamis Vehicle ola bilmez
+
+//Model-
+
+//FuelCapacity-
+
+//FuelFor1Km-
+
+//CurrentFuel-
+
+//ShowInfo()-butun detallari gosterecek
+
+namespace ConsoleApp4
+{
+    internal class Vehicleclass
+    {
+        public string color;
+        ////public int year;
+        ////public string model;
+        ////public int fuelCapacity;
+        ////public int fuelFor1Km;
+        ////public int currentFuel;
+        public Vehicleclass()
+        {
+             color = "red";
+            //year = 2020;
+            //model = "BMW";
+            //fuelCapacity = 40;
+            //fuelFor1Km = 10;
+            //currentFuel = 20;
+        }
+        static void Main(string[] args)
+        {
+            Vehicleclass Red= new Vehicleclass();
+            Console.WriteLine(Red.color);
+        }
+        //public void ShowInfo()
+        //{
+            
+        //}
+        //ShowInfo()-butun detallari gosterecek
+        //public class Vehicle(string color, int year, int fuelcapasity, int fuelFor1km, int currentFuel)
+        //{
+
+        //}
+    }
+}
